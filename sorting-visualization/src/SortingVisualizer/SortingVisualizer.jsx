96aee48e68a5c77f69a539ad5e353fd0ab85c629
Peter Huynh <peterlonghaihuynh25@gmail.com>
2022-05-25 14:48:26 -0400

    Update SortingVisualizer.jsx

--- sorting-visualization/src/SortingVisualizer/SortingVisualizer.jsx
+++ sorting-visualization/src/SortingVisualizer/SortingVisualizer.jsx
@@ -2,15 +2,33 @@ import React from 'react';
 import './SortingVisualizer.css';
 
 class SortingVisualizer extends React.Component {
+    // Constructor and initializes attributes.
+    // We only have one array as the only attribute.     
     constructor(props) {
-        super(props);
+        super(props);       // React.Component is required when using class-based component so we
+        // have to call parent constructor because our class is a a derived class.
+
+        // Attribute of our class which is an object that contains an array.                    
         this.state = {
             array: [],
         };
     }
+
+    resetArray() {
+        const temp = [];
+        for (let i = 0; i < 100; i++) {
+            temp.push(randomIntFrom(5, 1000));
+        }
+    }
+
     render() {
         return <div></div>
     }
+}
+
+// Return a random Int from interval (min, max).
+function randomIntFrom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export default SortingVisualizer
