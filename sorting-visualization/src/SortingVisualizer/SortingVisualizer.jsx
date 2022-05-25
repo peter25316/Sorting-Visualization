239296e7c7b8963b3054274d9a89a0ed866b32fa
Peter Huynh <peterlonghaihuynh25@gmail.com>
2022-05-25 13:27:32 -0400

    setting up array

--- sorting-visualization/src/SortingVisualizer/SortingVisualizer.jsx
+++ sorting-visualization/src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,3 +1,16 @@
 import React from 'react';
 import './SortingVisualizer.css';
 
+class SortingVisualizer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            array: [],
+        };
+    }
+    render() {
+        return <div></div>
+    }
+}
+
+export default SortingVisualizer
